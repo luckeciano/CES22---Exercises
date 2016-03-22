3e412b9f66e3239a38d233819057d213db8401c6
Luckeciano Melo <luckeciano@hotmail.com>
2016-03-22 16:27:40 -0300

    Initial Commit - Implementação Class Time

---
+++ Encapsulamento/src/Time.java
@@ -0,0 +1,41 @@
+
+public class Time {
+	//encapsulamento dos atributos: tornam-se private
+	private int hour;
+	private int minute;
+	
+	//implementa-se um construtor com o horário
+	public Time(int hour, int minute)
+	{
+		this.hour = hour;
+		this.minute = minute;
+	}	
+	//Para acessar os atributos privados, utilizamos um getter
+	public int getHour()
+	{
+		return this.hour;
+	}
+	public int getMinute()
+	{
+		return this.minute;
+	}
+	//Para modificar os atributos indiretamente, utilizamos setters
+	public void setHour(int hour)
+	{
+		this.hour = hour;
+	}
+	public void setMinute (int minute)
+	{
+		this.minute = minute;
+	}
+	// Funcionamento de getters e setters:
+	public static void main (String args[])
+	{
+		Time t = new Time(16, 30);
+		System.out.println("The time now is: " + t.getHour() + ":" + t.getMinute());
+		t.setHour(17);
+		t.setMinute(30);
+		System.out.println("Time has passed. Now is: " + t.getHour() + ":" + t.getMinute());
+	}
+
+}
