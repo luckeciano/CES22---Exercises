98acfbd8c5f71f8d3fc13f93457bdb3c73ca05fa
Luckeciano Melo <luckeciano@hotmail.com>
2016-03-22 16:43:54 -0300

    Explicação Encapsulamento e Vantagens

--- Encapsulamento/src/Time.java
+++ Encapsulamento/src/Time.java
@@ -20,13 +20,19 @@ public class Time {
 		return this.minute;
 	}
 	//Para modificar os atributos indiretamente, utilizamos setters
+	//Note que existe condições para os modificadores, o que não aconteceria
+	//se os objetos não estivessem encapsulados.
+	
 	public void setHour(int hour)
 	{
-		this.hour = hour;
+		if (hour >= 0 || hour <= 23)
+			this.hour = hour;
+		
 	}
 	public void setMinute (int minute)
 	{
-		this.minute = minute;
+		if (minute >=0 || minute <= 59)
+			this.minute = minute;
 	}
 	// Funcionamento de getters e setters:
 	public static void main (String args[])
@@ -39,3 +45,19 @@ public class Time {
 	}
 
 }
+
+//Existe uma série de vantagens de utilizar o encapsulamento
+//Facilidade de manutenção: podemos modificar a classe Time e seus atributos e os possíveis
+// usuários desta classe continuarão podendo utilizá-la independente desta alteração
+
+// Uso do objeto: o objeto t da classe Time é disponibilizado ao usuário com toda a sua funcionalidade
+// sem este precisar conhecer o funcionamento interno da classe
+
+// Implementações futuras com a classe Time são aceleradas, uma vez que os usuários
+// Não precisam, necessariamente, saber como os objetos são constituídos internamente.
+
+// Por fim, uma grande vantagem do encapsulamento é que você pode garantir a validade
+// das informações em setters. Na classe Time, os setters garantem que o horário armazenado
+// no objeto seja válido, algo que não aconteceria se os atributos fossem públicos.
+
+
