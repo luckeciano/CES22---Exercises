3f5e99a3bacac2e7e08c460f0a19be26f08d2a80
Luckeciano Melo <luckeciano@hotmail.com>
2016-04-18 02:08:58 -0300

    Comentários de funcionalidades

--- ProdutorConsumidor/src/Produtor.java
+++ ProdutorConsumidor/src/Produtor.java
@@ -1,15 +1,17 @@
-
+//Implementação da classe produtor
 public class Produtor extends Thread {
+	//Atributos do Produtor
 	private int idProdutor;
 	private Buffer pilha;
 	private int producaoTotal;
 	
+	//Construtor padrão
 	public Produtor (int id, Buffer p, int producaoTotal){
 		idProdutor = id;
 		pilha = p;
 		this.producaoTotal = producaoTotal;
 	}
-	
+	//Na execução, este tenta produzir elementos para inserir no buffer compartilhado
 	public void run(){
 		for (int i = 0; i < producaoTotal; i++){
 			pilha.set(idProdutor, i);
