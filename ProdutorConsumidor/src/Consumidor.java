ae4219ea0581ce09ed8f326627ac9a8291058a2d
Luckeciano Melo <luckeciano@hotmail.com>
2016-04-18 02:07:42 -0300

    Comentários de funcionalidades

--- ProdutorConsumidor/src/Consumidor.java
+++ ProdutorConsumidor/src/Consumidor.java
@@ -1,16 +1,19 @@
 //Implementação da classe consumidor
 public class Consumidor extends Thread {
 	
+	//Atributos do consumidor
 	private int idConsumidor;
 	private Buffer pilha;
 	private int totalConsumir;
 	
+	//Construtor padrão
 	public Consumidor (int id, Buffer p, int totalConsumir){
 		idConsumidor = id;
 		pilha = p;
 		this.totalConsumir = totalConsumir;
 	}
 	
+	//Na execução, este tentar consumir os itens do buffer comaprtilhado
 	public void run(){
 		for (int i = 0; i < totalConsumir; i++){
 			pilha.get(idConsumidor);
